c0d79f01bc9396a5b6d2ac7f6763ac7dcc221a48
wanve <wanve@wanvedeMacBook-Air.local>
2018-08-23 16:54:57 +0800

    update

--- DGArchive/DGArchive/Classes/WebView/WebViewController.m
+++ DGArchive/DGArchive/Classes/WebView/WebViewController.m
@@ -8,7 +8,7 @@
 @interface WebViewController()
 
 @property WebViewJavascriptBridge *bridge;
-@property (nonatomic , strong) UIWebView *webview ;
+@property (nonatomic , strong) WKWebView *wkwebView;
 
 @end
 
@@ -22,23 +22,24 @@
 //    self.fd_prefersNavigationBarHidden = YES;
 //    self.fd_interactivePopDisabled = YES;
     
-    UIWebView* webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
-    [self.view addSubview:webView];
-    
-    UIPanGestureRecognizer *swip = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(goBack)];
-    [webView addGestureRecognizer:swip];
-    self.webview = webView;
-    
-    [WebViewJavascriptBridge enableLogging];
-    _bridge = [WebViewJavascriptBridge bridgeForWebView:webView];
-    [_bridge setWebViewDelegate:self];
-    
-    [self loadWebView:webView];
+//    UIWebView* webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+//    [self.view addSubview:webView];
+//
+//    UIPanGestureRecognizer *swip = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(goBack)];
+//    [webView addGestureRecognizer:swip];
+//    self.wkwebview = webView;
+//
+//    [WebViewJavascriptBridge enableLogging];
+//    _bridge = [WebViewJavascriptBridge bridgeForWebView:webView];
+//    [_bridge setWebViewDelegate:self];
+//
+//    [self loadWebView:webView];
+    [self initWKWebView];
 }
 
 - (void)goBack
 {
-    [self.webview goBack];
+//    [self.wkwebview goBack];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -52,16 +53,74 @@
     [super viewWillDisappear:animated];
 }
 
-- (void)webViewDidStartLoad:(UIWebView *)webView {
-    PLog(@"webViewDidStartLoad");
+- (void)initWKWebView
+{
+    if(self.wkwebView == nil)
+    {
+        CGFloat statusBarH = [[UIApplication sharedApplication] statusBarFrame].size.height;
+        CGFloat navigationBarH = self.navigationController.navigationBar.frame.size.height;
+        CGFloat tabBarH = self.tabBarController.tabBar.bounds.size.height;
+        
+        WKWebView* webView = [[NSClassFromString(@"WKWebView") alloc] initWithFrame:CGRectMake(0, statusBarH + navigationBarH, SCREEN_WIDTH, SCREEN_HEIGHT - statusBarH - navigationBarH - tabBarH)];
+        webView.navigationDelegate = self;
+        [self.view addSubview:webView];
+        self.wkwebView = webView;
+    }
+    
+    if(_bridge == nil)
+    {
+        [WebViewJavascriptBridge enableLogging];
+        _bridge = [WebViewJavascriptBridge bridgeForWebView:self.wkwebView];
+        [_bridge setWebViewDelegate:self];
+        
+        [_bridge registerHandler:@"testObjcCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
+            NSLog(@"testObjcCallback called: %@", data);
+            responseCallback(@"Response from testObjcCallback");
+        }];
+        
+        [_bridge callHandler:@"testJavascriptHandler" data:@{ @"foo":@"before ready" }];
+    }
+    
+    [self loadWKWebView:self.wkwebView];
 }
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    PLog(@"webViewDidFinishLoad");
+#pragma mark - wkwebView delegate
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"webViewDidStartLoad");
 }
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSLog(@"webViewDidFinishLoad");
+}
+
+- (void)loadWKWebView:(WKWebView *)webView
+{
+//    NSURL *url = [NSURL URLWithString:self.url];
+//    [webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
+//- (void)webViewDidStartLoad:(UIWebView *)webView {
+//    PLog(@"webViewDidStartLoad");
+//}
+//
+//- (void)webViewDidFinishLoad:(UIWebView *)webView {
+//    PLog(@"webViewDidFinishLoad");
+//}
 
 - (void)loadWebView:(UIWebView*)webView
 {
+//    NSDictionary *properties = [[NSMutableDictionary alloc] init];
+//    [properties setValue:[share() getSessionKey] forKey:NSHTTPCookieValue];
+//    [properties setValue:@"sessionKey" forKey:NSHTTPCookieName];
+//    [properties setValue:[url host] forKey:NSHTTPCookieDomain];
+//    [properties setValue:[url path] forKey:NSHTTPCookiePath];
+//    [properties setValue:[NSDate dateWithTimeIntervalSinceNow:60*60] forKey:NSHTTPCookieExpires];
+//    NSHTTPCookie *cookie = [[NSHTTPCookie alloc] initWithProperties:properties];
+//    [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+//    [_webView loadRequest:[NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:15]];
+
+    
     UserModel *userModel = [[UserManager sharedUserManager] getUserModel];
 
 //    NSString *urlStr = [NSString stringWithFormat:@"http://121.15.203.82:9210/DMS_Phone/Login/QuickLogin.aspx?cmd={UserID:\"%@\",UserPsw:\"%@\"}", userModel.username, userModel.password];
