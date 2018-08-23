a54dda4122f110ebd2f53d16c28538f6c77b27f9
wanve <wanve@wanvedeMacBook-Air.local>
2018-08-23 10:59:48 +0800

    update

--- DGArchive/DGArchive/Classes/Others/Category/UIImage+image.m
+++ DGArchive/DGArchive/Classes/Others/Category/UIImage+image.m
@@ -26,4 +26,82 @@
     return image;
 }
 
++ (UIImage *)imageWithName:(NSString *)name {
+    
+    //如果长度为0 ,就直接返回
+    if (name.length<=0) {
+        
+        return nil;
+    }
+    
+    NSString *extension = @"png";
+    
+    NSArray *components = [name componentsSeparatedByString:@"."];
+    
+    if ([components count] >= 2) {
+        NSUInteger lastIndex = components.count - 1;
+        extension = [components objectAtIndex:lastIndex];
+        
+        name = [name substringToIndex:(name.length-(extension.length+1))];
+    }
+    
+    // 如果为Retina屏幕且存在对应图片，则返回Retina图片，否则查找普通图片
+    if ([UIScreen mainScreen].scale == 2.0) {
+        name = [name stringByAppendingString:@"@2x"];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:extension];
+        if (path != nil) {
+            return [UIImage imageWithContentsOfFile:path];
+        }
+    }
+    
+    if ([UIScreen mainScreen].scale == 3.0) {
+        
+        name = [name stringByAppendingString:@"@3x"];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:extension];
+        if (path != nil) {
+            
+            return [UIImage imageWithContentsOfFile:path];
+        }
+    }
+    //先清除之前的@1x,@2x,@3x
+    if ([name containsString:@"@1x"]||[name containsString:@"@2x"]||[name containsString:@"@3x"])
+    {
+        name = [name substringWithRange:NSMakeRange(0, name.length-3)];
+    }
+    
+    //没有符合要求的,有啥就用啥
+    NSString *path1 = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@@1x",name] ofType:extension];
+    
+    if (path1) {
+        
+        return [UIImage imageWithContentsOfFile:path1];
+    }
+    
+    NSString *path2 = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@@2x",name] ofType:extension];
+    
+    if (path2) {
+        
+        return [UIImage imageWithContentsOfFile:path2];
+    }
+    
+    NSString *path3 = [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@@3x",name] ofType:extension];
+    
+    if (path3) {
+        
+        return [UIImage imageWithContentsOfFile:path3];
+    }
+    
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:extension];
+    
+    if (path) {
+        
+        return [UIImage imageWithContentsOfFile:path];
+    }
+    
+    return nil;
+}
+
 @end
